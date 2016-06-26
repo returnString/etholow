63601cf52c24671a6ab77869d365e089e75fc4c9
Ruan Pearce-Authers <ruanpa@outlook.com>
2016-06-26 01:11:30 +0100

    Added "etholow" bin script

---
+++ bin/etholow.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+
+const co = require('co');
+const { Game } = require('../');
+
+co(function*()
+{
+	const game = new Game(process.argv[2]);
+	yield game.run();
+}).catch(err => console.error(err.stack));
